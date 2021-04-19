2ae3f8c01d4073db0cce570d08c7832a65f53873
Yusuf_teacher <45043321+stevenjon@users.noreply.github.com>
2021-04-19 22:33:30 +0500

    Fetch api modified

--- src/Functions/FetchApi.js
+++ src/Functions/FetchApi.js
@@ -1,6 +1,6 @@
 import { BaseUrl } from "../BaseUrl";
 
-const FetchApi = async (url, data = null, method, id = null) => {
+const FetchApi = async (url,  method, data = null, id = null) => {
   const uri = id ? url + "/" + id : url;
   try {
     return await axios({
